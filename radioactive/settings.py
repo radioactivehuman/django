918bace00fe90c82a8b41e8364f9c7bff9abb5af
radioactivehuman <79137045+radioactivehuman@users.noreply.github.com>
2021-02-25 02:11:09 -0700

    Update settings.py

--- radioactive/settings.py
+++ radioactive/settings.py
@@ -124,16 +124,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+ 
+                              
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
  
 #added manually
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR , "static"),
     
 ]
-
 # manageging media
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
